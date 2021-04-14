9f69343f202a7ef537953a48dda2e3fda08f674e
vitorgj <vitorgonc1983@hotmail.com>
2021-04-14 12:12:51 -0300

    fiz umas doideira ai, algumas deram certo

--- responsive-web/js/scripts.js
+++ responsive-web/js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
     // alert("Seja bem-vindo à nossa adega!")
+
+    $(".logo").mouseenter(function(){
+        $("img#logo").animate({
+            opacity: 0.50
+        })
+    })
+    $(".logo").mouseleave(function(){
+        $("img#logo").animate({
+            opacity: 1.0
+        })
+    })
 });
