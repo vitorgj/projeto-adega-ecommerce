eefd25e0f16f68b01abd07415fd9f480627f881c
vitorgj <vitorgonc1983@hotmail.com>
2021-04-15 11:56:08 -0300

    linkei as abas de navegação

--- responsive-web/js/scripts.js
+++ responsive-web/js/scripts.js
@@ -24,5 +24,55 @@ $(document).ready(function() {
             opacity: 1.0
         });
     });
+
+    $("ul.nav-container").addClass("unselectable")
+
+    $("li.btn-menu-nrg").click(function(){
+        $("div.content-nrg").addClass("style-nav")
+        $("div.content-nrg").slideToggle("slow");
+        $("li.btn-menu-nrg").mouseleave(function(){
+            $("div.content-nrg").slideUp("fast")
+        })
+    });
+
+    $("li.btn-menu-vod").click(function(){
+        $("div.content-vod").addClass("style-nav")
+        $("div.content-vod").slideToggle("slow");
+        $("li.btn-menu-vod").mouseleave(function(){
+            $("div.content-vod").slideUp("fast")
+        })
+    });
+
+    $("li.btn-menu-cer").click(function(){
+        $("div.content-cer").addClass("style-nav")
+        $("div.content-cer").slideToggle("slow");
+        $("li.btn-menu-cer").mouseleave(function(){
+            $("div.content-cer").slideUp("fast")
+        })
+    });
+
+    $("li.btn-menu-whi").click(function(){
+        $("div.content-whi").addClass("style-nav")
+        $("div.content-whi").slideToggle("slow");
+        $("li.btn-menu-whi").mouseleave(function(){
+            $("div.content-whi").slideUp("fast")
+        })
+    });
+
+    $("li.btn-menu-vin").click(function(){
+        $("div.content-vin").addClass("style-nav")
+        $("div.content-vin").slideToggle("slow");
+        $("li.btn-menu-vin").mouseleave(function(){
+            $("div.content-vin").slideUp("fast")
+        })
+    });
+
+    $("li.btn-menu-oth").click(function(){
+        $("div.content-oth").addClass("style-nav")
+        $("div.content-oth").slideToggle("slow");
+        $("li.btn-menu-oth").mouseleave(function(){
+            $("div.content-oth").slideUp("fast")
+        })
+    });
     
 });
